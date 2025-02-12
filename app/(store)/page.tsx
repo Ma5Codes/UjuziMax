c6dff40a16d8f9e4a9fa4238191ad5b29be04f06
Ma5Codes <117619833+Ma5Codes@users.noreply.github.com>
2025-02-12 13:51:06 +0300

    first? commit

---
+++ app/(store)/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "../../components/Hero";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-background">
+      <Hero />
+      Course grid goes here !!
+    </div>
+
+  );
+}
